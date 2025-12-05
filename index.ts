0b3d6c51f5d3a14a8dc39de16850bccca5820531
emmanuelrrbc <49891599-emmanuelrrbc@users.noreply.replit.com>
2025-12-05 16:48:19 +0000

    Initial commit

---
+++ index.ts
@@ -0,0 +1,8 @@
+import chalk from 'chalk'; 
+import animals from './public/animals.json' 
+
+const animal = process.argv[2] as keyof typeof animals;
+const animalData = animals[animal];
+if (animalData) {
+  console.log(chalk.green(`Nome científico: ${animalData.Nome_cientifico`))
+}
